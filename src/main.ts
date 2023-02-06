67b75f18bc78bfa4e5738a5f5b9009d073be06d5
Leonardo <lbvargas99@gmail.com>
2023-02-06 13:45:13 -0300

    feat: added use of PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+import PrimeVue from "primevue/config";
+
+const app = createApp(App);
+app.use(router);
+app.use(PrimeVue);
+app.mount("#app");
