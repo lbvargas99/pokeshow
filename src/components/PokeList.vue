b998b851225edd0b6f1ea79bf5784e2bdfc3d5b7
Leonardo <lbvargas99@gmail.com>
2023-02-08 11:46:44 -0300

    feat: created search filter of pokemons

--- src/components/PokeList.vue
+++ src/components/PokeList.vue
@@ -1,17 +1,25 @@
 <template>
   <LoaderGeneric v-if="isLoad" />
-  <div
-    class="grid flex flex-wrap justify-content-center align-items-center transition-delay-100"
-  >
+  <input
+    id="search"
+    type="text"
+    placeholder="Search pokemon"
+    v-model="searchValue"
+    autocomplete="off"
+  />
+  <div v-if="pokeList.length" class="transition-delay-100 container">
     <div
-      v-for="(pokemon, index) in list"
+      v-for="(pokemon, index) in pokeList"
       :key="index"
       @click="selectPokemon(getPokemonId(pokemon.url))"
-      class="col-3 cursor-pointer card-radius w-12rem h-9rem m-2"
+      class="col-3 cursor-pointer card h-10rem m-2"
     >
       <img :src="getPokemonImage(getPokemonId(pokemon.url))" />
       <p>{{ pokemon.name }}</p>
     </div>
+  </div>
+  <div v-else class="container">
+    No pokemons found for this search: {{ searchValue }}
   </div>
 </template>
 
@@ -21,7 +29,7 @@ import {
   getPokemonImageUrl,
   getIdFromUrlPokemon,
 } from "@/services/pokemon-service";
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { useRouter } from "vue-router";
 
 import LoaderGeneric from "@/components/LoaderGeneric.vue";
@@ -34,6 +42,7 @@ export default {
     const list = ref([]);
     const router = useRouter();
     const isLoad = ref(false);
+    const searchValue = ref("");
 
     const getPokemonId = (url: any) => getIdFromUrlPokemon(url);
 
@@ -42,6 +51,18 @@ export default {
     //There is just one 'id' 'cause the prop has same name of param id = pokemon/:id
     const selectPokemon = (id: any) =>
       router.push({ name: "pokemon", params: { id } });
+
+    //Performs the search with searchValue from input and filter on array
+    const pokeList = computed(() => {
+      if (searchValue.value.trim().length >= 3) {
+        return list.value.filter((poke: any) =>
+          poke.name
+            .toLowerCase()
+            .includes(searchValue.value.trim().toLocaleLowerCase())
+        );
+      }
+      return list.value;
+    });
 
     //Consume service on initial component
     onMounted(() => {
@@ -60,6 +81,8 @@ export default {
       getPokemonId,
       selectPokemon,
       isLoad,
+      searchValue,
+      pokeList,
     };
   },
 };
@@ -68,12 +91,35 @@ export default {
 <style scoped>
 @import "primeflex/primeflex.min.css";
 
-.card-radius {
-  border: 1px solid;
-  border-radius: 5px;
+#search {
+  width: 450px;
+  margin: 10px;
+  padding: 10px;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+    rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+  border-radius: 20px;
+  border: none;
 }
 
-.card-radius:hover {
+#search:focus {
+  outline: none;
+}
+
+.container {
+  width: 700px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.card {
+  border-radius: 5px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  width: 9rem !important;
+}
+
+.card:hover {
   background: radial-gradient(
     328px at 2.9% 15%,
     rgba(191, 224, 251, 1) 0%,
